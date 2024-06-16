b417bcd22c4a30b6c73e35d04674d17fd821cd68
Richard Sun <sunzh231@gmail.com>
2024-06-16 01:06:04 +0000

    refine code

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,12 @@
-// import { defineConfig } from 'vite'
-// import { resolve } from 'path'
-// import vue from '@vitejs/plugin-vue'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   resolve: {
-//     alias: {
-//       '@': resolve(__dirname, 'frontend')
-//     }
-//   },
-//   server: {
-//     port: 4200,
-//     proxy: {
-//       '/api/': {
-//         target: 'http://127.0.0.1:54321/api/',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       },
-//     }
-//   },
-//   base: '/templates/',
-//   build: {
-//     outDir: './templates',
-//   }
-// })
-
-import { resolve } from 'path'
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, 'frontend')
+      "@": resolve(__dirname, "frontend"),
     },
   },
   build: {
